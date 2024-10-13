cfcb03d72402473e111bd410cae271d7ea465693
minvistar <rlarnfk135@naver.com>
2024-10-14 05:54:12 +0900

    .

--- packages/ui/src/layouts/dashboard/sidebar/nav/nav.tsx
+++ packages/ui/src/layouts/dashboard/sidebar/nav/nav.tsx
@@ -1,13 +1,24 @@
-import { Box, ButtonBase, Stack, SxProps } from "@mui/material";
+import { Box, ButtonBase, ButtonBaseProps, Collapse, Stack, SxProps } from "@mui/material";
 import { Theme, useTheme } from "@mui/material/styles";
+import { Icon } from "@iconify/react";
 import { NavData, BranchData, LeafData } from "../types";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { varAlpha } from "../../../../util";
+import { useCallback, useMemo, useState } from "react";
 
 const navToken = {
   ul: {
     gap: "--nav-ul-gap",
+  },
+  leaf: {
+    iconSize: "--nav-leaf-icon-size",
+    minHeight: "--nav-leaf-height",
+    color: "--nav-leaf-color",
+    activeColor: "--nav-leaf-active-color",
+    bgcolor: "--nav-leaf-bgcolor",
+    activeBgcolor: "--nav-leaf-active-bgcolor",
+    hoverActiveBgcolor: "--nav-leaf-hover-active-bgcolor",
   },
 } as const;
 
@@ -25,6 +36,13 @@ export function Nav({ data, sx }: NavProps) {
         px: 2,
         flex: "1 1 auto",
         [navToken.ul.gap]: theme.spacing(0.5),
+        [navToken.leaf.iconSize]: "24px",
+        [navToken.leaf.minHeight]: "44px",
+        [navToken.leaf.color]: theme.vars.palette.text.secondary,
+        [navToken.leaf.activeColor]: theme.vars.palette.primary.main,
+        [navToken.leaf.bgcolor]: "transparent",
+        [navToken.leaf.activeBgcolor]: varAlpha(theme.vars.palette.primary.mainChannel, 0.08),
+        [navToken.leaf.hoverActiveBgcolor]: varAlpha(theme.vars.palette.primary.mainChannel, 0.16),
         ...sx,
       }}
     >
@@ -102,7 +120,7 @@ function Trunk({ data }: TrunkProps) {
   return (
     <Li>
       {isLeafData(data) ? (
-        <Leaf data={data} active={pathname.startsWith(data.href)} depth={false} />
+        <Leaf data={data} active={pathname.startsWith(data.href)} arrow="none" />
       ) : (
         <Branch data={data} />
       )}
@@ -122,23 +140,52 @@ type BranchProps = {
 
 function Branch({ data }: BranchProps) {
   const pathname = usePathname();
+
+  const [open, setOpen] = useState(data.items.some((item) => pathname.startsWith(item.href)));
   const theme = useTheme();
+
+  const handleOpen = useCallback(() => {
+    setOpen(!open);
+  }, [open, setOpen]);
+
+  const parentActive = useMemo(() => data.items.some((item) => pathname.startsWith(item.href)), [data, pathname]);
 
   return (
     <>
       <Leaf
         data={data}
-        active={data.items.some((item) => pathname.startsWith(item.href))}
-        depth={false}
-        sx={{ mb: `var(${navToken.ul.gap})` }}
+        active={parentActive}
+        arrow={open ? "downward" : "forward"}
+        sx={{
+          mb: `var(${navToken.ul.gap})`,
+          ...(!parentActive &&
+            open && {
+              [navToken.leaf.color]: theme.vars.palette.text.primary,
+              [navToken.leaf.bgcolor]: theme.vars.palette.action.selected,
+              [navToken.leaf.hoverActiveBgcolor]: theme.vars.palette.action.selected,
+            }),
+        }}
+        onClick={handleOpen}
       />
-      <Ul>
-        {data.items.map((item) => (
-          <Li key={item.id}>
-            <Leaf data={item} active={pathname.startsWith(item.href)} depth={true} />
-          </Li>
-        ))}
-      </Ul>
+      <Collapse in={open} mountOnEnter unmountOnExit>
+        <Ul sx={{ pl: `calc(${theme.spacing(1.5)} + var(${navToken.leaf.iconSize}))` }}>
+          {data.items.map((item) => (
+            <Li key={item.id}>
+              <Leaf
+                data={item}
+                active={pathname.startsWith(item.href)}
+                arrow="none"
+                sx={{
+                  [navToken.leaf.minHeight]: "36px",
+                  [navToken.leaf.activeColor]: theme.vars.palette.text.primary,
+                  [navToken.leaf.activeBgcolor]: theme.vars.palette.action.selected,
+                  [navToken.leaf.hoverActiveBgcolor]: theme.vars.palette.action.selected,
+                }}
+              />
+            </Li>
+          ))}
+        </Ul>
+      </Collapse>
     </>
   );
 }
@@ -148,11 +195,11 @@ function Branch({ data }: BranchProps) {
 type LeafProps = {
   data: Omit<LeafData, "href"> & Partial<Pick<LeafData, "href">>;
   active: boolean;
-  depth: boolean;
+  arrow: "forward" | "downward" | "none";
   sx?: SxProps<Theme>;
-};
+} & ButtonBaseProps;
 
-function Leaf({ data, active, depth, sx }: LeafProps) {
+function Leaf({ data, active, arrow, sx, ...rest }: LeafProps) {
   const theme = useTheme();
 
   return (
@@ -161,34 +208,35 @@ function Leaf({ data, active, depth, sx }: LeafProps) {
       {...(data.href && { href: data.href })}
       sx={{
         width: "100%",
-        minHeight: "44px",
+        minHeight: `var(${navToken.leaf.minHeight})`,
         pt: theme.spacing(0.5),
         pr: theme.spacing(1),
         pb: theme.spacing(0.5),
         pl: theme.spacing(1.5),
         borderRadius: `${theme.shape.borderRadius}px`,
-        color: theme.vars.palette.text.secondary,
+        bgcolor: `var(${navToken.leaf.bgcolor})`,
+        color: `var(${navToken.leaf.color})`,
         textAlign: "start",
         "&:hover": {
           bgcolor: theme.vars.palette.action.hover,
         },
         ...(active && {
-          color: theme.vars.palette.primary.main,
-          bgcolor: varAlpha(theme.vars.palette.primary.mainChannel, 0.08),
+          color: `var(${navToken.leaf.activeColor})`,
+          bgcolor: `var(${navToken.leaf.activeBgcolor})`,
           "&:hover": {
-            bgcolor: varAlpha(theme.vars.palette.primary.mainChannel, 0.16),
+            bgcolor: `var(${navToken.leaf.hoverActiveBgcolor})`,
           },
         }),
-        ml: depth ? `calc(${theme.spacing(1.5)} + 24px)` : null,
         ...sx,
       }}
+      {...rest}
     >
       {data.icon ? (
         <Box
           component="span"
           sx={{
-            width: "24px",
-            height: "24px",
+            width: `var(${navToken.leaf.iconSize})`,
+            height: `var(${navToken.leaf.iconSize})`,
             margin: theme.spacing(0, 1.5, 0, 0),
           }}
         >
@@ -213,6 +261,16 @@ function Leaf({ data, active, depth, sx }: LeafProps) {
           {data.title}
         </Box>
       </Box>
+      {arrow !== "none" ? (
+        <Icon
+          icon={arrow === "downward" ? "eva:arrow-ios-downward-fill" : "eva:arrow-ios-forward-fill"}
+          style={{
+            width: "16px",
+            height: "16px",
+            marginLeft: "6px",
+          }}
+        />
+      ) : null}
     </ButtonBase>
   );
 }
