d792e84ce328b4ad12e95bf81fec4349078209bd
Xiel0112 <shinhw1127@gmail.com>
2024-11-01 22:41:28 +0900

    .

---
+++ apps/web/app/_core/webtoon/post/ui/wt-post-rich-card.tsx
@@ -0,0 +1,88 @@
+import { forwardRef, useMemo } from "react";
+import { GravityUiCircleCheckFillIcon, NineteenCircleIcon, OverviewCard } from "@pency/ui/components";
+import { Age, CreationType, CREATION_TYPE_LABEL, Pair, PAIR_LABEL } from "../const";
+import { Genre, GENRE_LABEL } from "_core/webtoon/const";
+
+type Props = {
+  data: {
+    postId: string;
+    thumbnail: string;
+    age: Age;
+    price: number;
+    purchased: boolean;
+    creationType: CreationType;
+    pair: Pair;
+    genre: Genre;
+    title: string;
+    channel: {
+      channelId: string;
+      avatar: string;
+      name: string;
+    };
+  };
+};
+
+export const WT_Post_OverviewCard = forwardRef<HTMLDivElement, Props>(({ data }, ref) => {
+  const ageIcon = useMemo(() => {
+    if (data.age === "NINETEEN") {
+      return <NineteenCircleIcon fontSize="small" />;
+    }
+
+    return null;
+  }, [data]);
+
+  const purchasedIcon = useMemo(() => {
+    if (data.purchased) {
+      return <GravityUiCircleCheckFillIcon />;
+    }
+
+    return null;
+  }, [data]);
+
+  return (
+    <OverviewCard
+      ref={ref}
+      slots={{
+        overlayElement: <OverviewCard.OverlayAnchor href={`/webtoon/post/${data.postId}`} />,
+        thumbnail: (
+          <OverviewCard.Thumbnail
+            slots={{
+              image: <OverviewCard.Thumbnail.Image src={data.thumbnail} sx={{ aspectRatio: "16/9" }} />,
+              topEnds: ageIcon,
+            }}
+          />
+        ),
+        labels: (
+          <>
+            {data.price && (
+              <OverviewCard.Label variant="soft" color="success" slots={{ startIcon: purchasedIcon }}>
+                {data.price}P
+              </OverviewCard.Label>
+            )}
+            <OverviewCard.Label variant="soft" color="secondary">
+              {CREATION_TYPE_LABEL[data.creationType]}
+            </OverviewCard.Label>
+            <OverviewCard.Label variant="soft" color="warning">
+              {PAIR_LABEL[data.pair]}
+            </OverviewCard.Label>
+            <OverviewCard.Label variant="soft" color="warning">
+              {GENRE_LABEL[data.genre]}
+            </OverviewCard.Label>
+          </>
+        ),
+        avatarLink: (
+          <OverviewCard.AvatarLink
+            href={`/channel/${data.channel.channelId}`}
+            slots={{
+              avatar: <OverviewCard.AvatarLink.Avatar src={data.channel.avatar} />,
+            }}
+          />
+        ),
+        title: <OverviewCard.Title>{data.title}</OverviewCard.Title>,
+        nameLink: (
+          <OverviewCard.NameLink href={`/channel/${data.channel.channelId}`}>{data.channel.name}</OverviewCard.NameLink>
+        ),
+      }}
+    />
+  );
+});
---
+++ apps/web/app/_core/webtoon/series/ui/wt-series-rich-card.tsx
@@ -0,0 +1,59 @@
+import { Genre, GENRE_LABEL } from "_core/webtoon/const";
+import { forwardRef } from "react";
+import { COMPLETION_STATE_LABEL, CompletionState } from "../const";
+import { OverviewCard } from "@pency/ui/components";
+
+type Props = {
+  data: {
+    seriesId: string;
+    thumbnail: string;
+    genre: Genre;
+    completionState: CompletionState;
+    title: string;
+    channel: {
+      channelId: string;
+      avatar: string;
+      name: string;
+    };
+  };
+};
+
+export const WT_Series_OverviewCard = forwardRef<HTMLDivElement, Props>(({ data }, ref) => {
+  return (
+    <OverviewCard
+      ref={ref}
+      slots={{
+        overlayElement: <OverviewCard.OverlayAnchor href={`/webtoon/series/${data.seriesId}`} />,
+        thumbnail: (
+          <OverviewCard.Thumbnail
+            slots={{
+              image: <OverviewCard.Thumbnail.Image src={data.thumbnail} sx={{ aspectRatio: "16/9" }} />,
+            }}
+          />
+        ),
+        labels: (
+          <>
+            <OverviewCard.Label variant="soft" color="warning">
+              {GENRE_LABEL[data.genre]}
+            </OverviewCard.Label>
+            <OverviewCard.Label variant="soft" color="info">
+              {COMPLETION_STATE_LABEL[data.completionState]}
+            </OverviewCard.Label>
+          </>
+        ),
+        avatarLink: (
+          <OverviewCard.AvatarLink
+            href={`/channel/${data.channel.channelId}`}
+            slots={{
+              avatar: <OverviewCard.AvatarLink.Avatar src={data.channel.avatar} />,
+            }}
+          />
+        ),
+        title: <OverviewCard.Title>{data.title}</OverviewCard.Title>,
+        nameLink: (
+          <OverviewCard.NameLink href={`/channel/${data.channel.channelId}`}>{data.channel.name}</OverviewCard.NameLink>
+        ),
+      }}
+    />
+  );
+});
