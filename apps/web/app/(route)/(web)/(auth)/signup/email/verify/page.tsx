761b4872c53eeaf595012436ee8855328753b19f
Xiel0112 <shinhw1127@gmail.com>
2024-11-04 23:04:11 +0900

    .

---
+++ apps/web/app/(route)/(web)/(auth)/signup/email/verify/page.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  IconButton,
+  InputAdornment,
+  Link,
+  Stack,
+  TextField,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { EvaEyeFillIcon, EvaEyeOffFillIcon } from "@pency/ui/components";
+import { useBooleanState } from "@pency/util";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+  email: z.string().min(1, "이메일 주소를 입력해 주세요.").email("이메일 주소를 정확하게 입력해 주세요."),
+});
+type Schema = z.infer<typeof schema>;
+
+export default function Page() {
+  const theme = useTheme();
+
+  const { control, handleSubmit } = useForm<Schema>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      email: "",
+    },
+    mode: "onTouched",
+  });
+
+  const onSubmit = (data: Schema) => {
+    console.log(data);
+  };
+
+  return (
+    <Box>
+      <Stack spacing={4}>
+        <Typography variant="h4">이메일 인증</Typography>
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
+          <Stack spacing={3}>
+            <Typography variant="body2" color={theme.vars.palette.text.secondary}>
+              '인증 메일 받기' 버튼을 누르면 등록한 이메일 주소로 인증 메일을 보내드려요. 24시간 안에 링크를 열어 이메일
+              인증을 완료해주세요.
+            </Typography>
+
+            <Typography variant="body2" color={theme.vars.palette.text.secondary}>
+              인증 메일을 받지 못했거나, 이메일 주소를 바꾸려면 아래 입력 칸에 이메일 주소를 올바르게 입력하고 다시
+              '인증 메일 받기' 버튼을 눌러주세요.
+            </Typography>
+
+            <Controller
+              control={control}
+              name="email"
+              render={({ field, fieldState: { error } }) => (
+                <TextField
+                  {...field}
+                  variant="filled"
+                  fullWidth
+                  type="email"
+                  label="이메일"
+                  helperText={error?.message}
+                  error={!!error}
+                />
+              )}
+            />
+
+            <Button type="submit" variant="soft" color="primary" size="large" fullWidth>
+              인증 메일 받기
+            </Button>
+          </Stack>
+        </form>
+      </Stack>
+    </Box>
+  );
+}
--- apps/web/app/(route)/(web)/(auth)/signup/page.tsx
+++ apps/web/app/(route)/(web)/(auth)/signup/page.tsx
@@ -27,11 +27,15 @@ export default function Page() {
   const onSubmit = (provider?: "google" | "apple" | "naver") => {
     switch (provider) {
       case "google":
-        // google.com
+        router.push("https://www.google.co.kr/");
         break;
       case "apple":
+        router.push(
+          "https://www.apple.com/kr/store?afid=p238%7CsiADh6hbK-dc_mtid_18707vxu38484_pcrid_719546863096_pgrid_16348496961_pntwk_g_pchan__pexid__ptid_kwd-304236833465_&cid=aos-kr-kwgo-Brand--slid---product-",
+        );
         break;
       case "naver":
+        router.push("https://www.naver.com/");
         break;
       default:
         router.push("/signup/email");
@@ -44,8 +48,8 @@ export default function Page() {
     <Box>
       <Stack spacing={4}>
         <Typography variant="h4">펜시 회원가입</Typography>
-        <form noValidate>
-          <Stack spacing={2.5}>
+        <Stack spacing={1}>
+          <form noValidate>
             <Stack spacing={1}>
               <Button variant="soft" startIcon={<BrandGoogleIcon onClick={handleSubmit(() => onSubmit("google"))} />}>
                 구글 회원가입
@@ -62,9 +66,7 @@ export default function Page() {
               <Button variant="soft" startIcon={<IcOutlineEmailIcon />} onClick={handleSubmit(() => onSubmit())}>
                 이메일 회원가입
               </Button>
-            </Stack>
 
-            <Stack spacing={1.5}>
               <Controller
                 control={control}
                 name="age"
@@ -75,33 +77,35 @@ export default function Page() {
                   />
                 )}
               />
-              <Divider />
-              <Stack direction="row" spacing={1}>
-                <Link href="/login" variant="subtitle2">
-                  로그인
-                </Link>
-                <Typography variant="subtitle2" color={theme.vars.palette.text.secondary}>
-                  ·
-                </Typography>
-                <Link href="/account/find" variant="subtitle2">
-                  계정 찾기
-                </Link>
-              </Stack>
-              <Divider />
-              <Typography variant="subtitle2" color={theme.vars.palette.text.secondary}>
-                SNS로 로그인 및 회원가입 시 펜시의{" "}
-                <Link href="/terms" target="_blank" variant="subtitle2" underline="always" color="inherit">
-                  이용약관
-                </Link>
-                {"과 "}
-                <Link href="/privacy" target="_blank" variant="subtitle2" underline="always" color="inherit">
-                  개인정보 수집 및 이용
-                </Link>
-                에 동의한 것으로 간주합니다.
-              </Typography>
             </Stack>
+          </form>
+          <Stack spacing={1.5}>
+            <Divider />
+            <Stack direction="row" spacing={1}>
+              <Link href="/login" variant="subtitle2">
+                로그인
+              </Link>
+              <Typography variant="subtitle2" color={theme.vars.palette.text.secondary}>
+                ·
+              </Typography>
+              <Link href="/account/find" variant="subtitle2">
+                계정 찾기
+              </Link>
+            </Stack>
+            <Divider />
+            <Typography variant="subtitle2" color={theme.vars.palette.text.secondary}>
+              SNS로 로그인 및 회원가입 시 펜시의{" "}
+              <Link href="/terms" target="_blank" variant="subtitle2" underline="always" color="inherit">
+                이용약관
+              </Link>
+              {"과 "}
+              <Link href="/privacy" target="_blank" variant="subtitle2" underline="always" color="inherit">
+                개인정보 수집 및 이용
+              </Link>
+              에 동의한 것으로 간주합니다.
+            </Typography>
           </Stack>
-        </form>
+        </Stack>
       </Stack>
     </Box>
   );
