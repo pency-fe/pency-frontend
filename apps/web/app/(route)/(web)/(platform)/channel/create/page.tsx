abec3d718dfe21713676df3cffb0a4bcfaa1bc0e
Xiel0112 <shinhw1127@gmail.com>
2024-11-07 16:37:00 +0900

    .

---
+++ apps/web/app/(route)/(web)/(platform)/channel/create/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Stack, Typography } from "@mui/material";
+import { CH_Create_Form } from "_core/channel/boundary/ch-form";
+
+export default function Page() {
+  return (
+    <Stack spacing={4}>
+      <Typography variant="h4">새 채널 만들기</Typography>
+      <form noValidate>
+        <Stack spacing={4}>
+          <CH_Create_Form>
+            <CH_Create_Form.Title />
+            <CH_Create_Form.Description />
+            <CH_Create_Form.Url />
+            <CH_Create_Form.SubmitButton />
+          </CH_Create_Form>
+        </Stack>
+      </form>
+    </Stack>
+  );
+}
--- apps/web/app/_core/channel/boundary/ch-form.tsx
+++ apps/web/app/_core/channel/boundary/ch-form.tsx
@@ -1,4 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, InputAdornment, TextField, useTheme } from "@mui/material";
 import { ReactNode } from "react";
 import { Controller, FormProvider, useForm, useFormContext } from "react-hook-form";
 import { z } from "zod";
@@ -6,8 +7,13 @@ import { z } from "zod";
 // ----------------------------------------------------------------------
 
 const schema = z.object({
-  title: z.string().min(1).max(45),
-  description: z.string().max(200).optional(),
+  title: z.string().min(1, "채널 제목을 입력해 주세요.").max(45, "최대 45자 이내로 입력해 주세요."),
+  description: z.string().max(200, "최대 200자 이내로 입력해 주세요.").optional(),
+  url: z
+    .string()
+    .regex(/^[a-z0-9-]+$/, "URL은 영문 소문자, 숫자, 대시(-)만 입력할 수 있어요.")
+    .min(6, "최소 6자 이상 입력해 주세요.")
+    .max(45, "최대 45자 이내로 입력해 주세요."),
 });
 
 type Schema = z.infer<typeof schema>;
@@ -23,6 +29,8 @@ const CH_Create_Form_Fn = ({ children }: CH_Create_Form_Fn_Props) => {
     resolver: zodResolver(schema),
     defaultValues: {
       title: "",
+      description: "",
+      url: "",
     },
     mode: "onTouched",
   });
@@ -32,6 +40,16 @@ const CH_Create_Form_Fn = ({ children }: CH_Create_Form_Fn_Props) => {
 
 const CreateSubmitFn = () => {
   const { handleSubmit } = useFormContext();
+
+  const onSubmit = (data: Schema) => {
+    console.log(data);
+  };
+
+  return (
+    <Button type="submit" variant="contained" color="primary" size="large" onClick={handleSubmit(() => onSubmit)}>
+      새 채널 만들기
+    </Button>
+  );
 };
 
 // ----------------------------------------------------------------------
@@ -44,14 +62,108 @@ const CreateSubmitFn = () => {
 
 const TitleFn = () => {
   const { control } = useFormContext<Schema>();
+  const theme = useTheme();
 
-  <Controller control={control} name="title" render={() => <></>} />;
+  return (
+    <Controller
+      control={control}
+      name="title"
+      render={({ field, fieldState: { error } }) => (
+        <TextField
+          {...field}
+          variant="outlined"
+          fullWidth
+          type="text"
+          label="채널 제목"
+          required
+          helperText={error ? error.message : "최대 45자 이내로 입력해 주세요."}
+          error={!!error}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end" sx={{ fontSize: theme.typography.caption }}>
+                {field.value?.length}/45
+              </InputAdornment>
+            ),
+          }}
+        />
+      )}
+    />
+  );
 };
 
-const DescriptionFn = () => {};
+// ----------------------------------------------------------------------
 
-const UrlFn = () => {};
+const DescriptionFn = () => {
+  const { control } = useFormContext<Schema>();
+  const theme = useTheme();
 
-export const CH_Create_Form = Object.assign(CH_Create_Form_Fn, {});
+  return (
+    <Controller
+      control={control}
+      name="description"
+      render={({ field, fieldState: { error } }) => (
+        <TextField
+          {...field}
+          variant="outlined"
+          fullWidth
+          multiline
+          minRows={5}
+          type="text"
+          label="채널 설명"
+          helperText={error ? error.message : "최대 200자 이내로 입력해 주세요."}
+          error={!!error}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end" sx={{ fontSize: theme.typography.caption }}>
+                {field.value?.length}/200
+              </InputAdornment>
+            ),
+          }}
+        />
+      )}
+    />
+  );
+};
+
+// ----------------------------------------------------------------------
+
+const UrlFn = () => {
+  const { control } = useFormContext<Schema>();
+  const theme = useTheme();
+
+  return (
+    <Controller
+      control={control}
+      name="url"
+      render={({ field, fieldState: { error } }) => (
+        <TextField
+          {...field}
+          variant="outlined"
+          fullWidth
+          type="text"
+          label="채널 URL"
+          required
+          helperText={error ? error.message : "최대 45자 이내로 입력해 주세요."}
+          error={!!error}
+          InputProps={{
+            startAdornment: <InputAdornment position="start">pency.com/@</InputAdornment>,
+            endAdornment: (
+              <InputAdornment position="end" sx={{ fontSize: theme.typography.caption }}>
+                {field.value?.length}/45
+              </InputAdornment>
+            ),
+          }}
+        />
+      )}
+    />
+  );
+};
+
+export const CH_Create_Form = Object.assign(CH_Create_Form_Fn, {
+  Title: TitleFn,
+  Description: DescriptionFn,
+  Url: UrlFn,
+  SubmitButton: CreateSubmitFn,
+});
 
 // export const CH_Update_Form = Object.assign(CH_Update_Form_Fn, {});
