56449448cc56cc463eb40ae0b645d2e2656f50d4
Xiel0112 <shinhw1127@gmail.com>
2024-11-08 23:12:02 +0900

    .

--- apps/web/app/_route/(web)/(auth)/signup/email/resend/page/page.tsx
+++ apps/web/app/_route/(web)/(auth)/signup/email/resend/page/page.tsx
@@ -5,6 +5,15 @@ import { useSearchParams } from "next/navigation";
 import { FormEvent, useState } from "react";
 
 export function ResendPage() {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const provisionUserId = useSearchParams().get("provisionUserId");
+    if (provisionUserId) {
+      await handleResendClick({ provisionUserId });
+    }
+  };
+
   return (
     <Box>
       <Stack spacing={4}>
@@ -12,6 +21,8 @@ export function ResendPage() {
         <form onSubmit={handleSubmit} noValidate>
           <Stack spacing={4}>
             <TextField variant="filled" fullWidth type="email" label="이메일" value={email} disabled />
+
+            {error && <Typography>{error}</Typography>}
 
             <Button type="submit" variant="soft" color="primary" size="large" fullWidth>
               인증 메일 재전송
