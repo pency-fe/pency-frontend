e0230b592cf09bd79ea339e8c6156a63eb38d0dc
Xiel0112 <shinhw1127@gmail.com>
2024-11-09 22:06:46 +0900

    .

--- apps/web/app/_route/(web)/(auth)/signup/email/verify/page/page.tsx
+++ apps/web/app/_route/(web)/(auth)/signup/email/verify/page/page.tsx
@@ -1,9 +1,35 @@
 "use client";
 
 import { Box, Button, Stack, Typography, useTheme } from "@mui/material";
+import { toast } from "@pency/ui/components";
+import { useVerify } from "_core/auth/provision-user";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export function VerifyPage() {
   const theme = useTheme();
+
+  const router = useRouter();
+  const token = useSearchParams().get("token");
+  const { mutate } = useVerify();
+
+  if (token === null || !token.length) {
+    router.replace("/signup/email/not-verify");
+    return;
+  }
+
+  const handleHomeClick = (data: { token: string }) => {
+    mutate(data, {
+      onSuccess: () => {
+        router.push("/");
+      },
+      onError: (error) => {
+        if (error.code === "EXPIRED_EMAIL_TOKEN") {
+          toast.error(error.message);
+          return;
+        }
+      },
+    });
+  };
 
   return (
     <Box>
@@ -14,7 +40,15 @@ export function VerifyPage() {
             이메일 인증이 완료됐어요.
           </Typography>
 
-          <Button type="submit" variant="soft" color="primary" size="large" fullWidth>
+          <Button
+            variant="soft"
+            color="primary"
+            size="large"
+            fullWidth
+            onClick={() => {
+              handleHomeClick({ token });
+            }}
+          >
             펜시 홈으로 가기
           </Button>
         </Stack>
