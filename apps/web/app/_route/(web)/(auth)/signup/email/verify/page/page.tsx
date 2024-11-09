b8ec43f5d172f419cf1177ff02a6a4a882ef2bdc
Xiel0112 <shinhw1127@gmail.com>
2024-11-09 21:05:33 +0900

    .

--- apps/web/app/_route/(web)/(auth)/signup/email/verify/page/page.tsx
+++ apps/web/app/_route/(web)/(auth)/signup/email/verify/page/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Box, Button, Stack, TextField, Typography, useTheme } from "@mui/material";
-import { Controller, useForm } from "react-hook-form";
-import { z } from "zod";
+import { Box, Button, Stack, Typography, useTheme } from "@mui/material";
 
 export function VerifyPage() {
   const theme = useTheme();
