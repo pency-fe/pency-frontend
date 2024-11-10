48727b21284a609376a35221b1275151dafd00df
Xiel0112 <shinhw1127@gmail.com>
2024-11-10 21:48:35 +0900

    .

--- apps/web/app/_route/(web)/(auth)/layout/left.tsx
+++ apps/web/app/_route/(web)/(auth)/layout/left.tsx
@@ -1,9 +1,18 @@
 "use client";
 
 import { IcomoonFreeYoutube2Icon } from "@pency/ui/components";
+import { useRouter } from "next/navigation";
 
 export default function Left() {
+  const router = useRouter();
+  const handleHomeClick = () => {
+    router.push("/");
+  };
+
   return (
-    <IcomoonFreeYoutube2Icon sx={{ width: "fit-content", height: "24px", overflow: "unset", cursor: "pointer" }} />
+    <IcomoonFreeYoutube2Icon
+      onClick={handleHomeClick}
+      sx={{ width: "fit-content", height: "24px", overflow: "unset", cursor: "pointer" }}
+    />
   );
 }
