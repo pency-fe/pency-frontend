cd20d814455ab81258c7f975a37fe8d049d14ba2
Xiel0112 <shinhw1127@gmail.com>
2024-11-10 21:11:51 +0900

    .

--- apps/web/app/_route/(web)/(platform)/layout/header/right.tsx
+++ apps/web/app/_route/(web)/(platform)/layout/header/right.tsx
@@ -1,3 +1,46 @@
+import { Avatar, IconButton, InputAdornment, inputBaseClasses, Stack, TextField, useTheme } from "@mui/material";
+import { IcRoundSearchIcon, MingcuteNotificationLineIcon } from "@pency/ui/components";
+
 export function Right() {
-  return <>오른쪽</>;
+  const theme = useTheme();
+
+  return (
+    <Stack flexDirection="row" alignItems="center" spacing={1}>
+      <TextField
+        variant="filled"
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IcRoundSearchIcon />
+            </InputAdornment>
+          ),
+        }}
+        sx={{
+          [`& .${inputBaseClasses.root}`]: { height: 36 },
+          [`& .${inputBaseClasses.input}`]: { paddingLeft: 1.5, paddingY: 0 },
+          [theme.breakpoints.down("sm")]: {
+            display: "none",
+          },
+        }}
+      />
+
+      <IconButton
+        sx={{
+          [theme.breakpoints.up("sm")]: {
+            display: "none",
+          },
+        }}
+      >
+        <IcRoundSearchIcon />
+      </IconButton>
+
+      <IconButton>
+        <MingcuteNotificationLineIcon />
+      </IconButton>
+      <Avatar
+        src="https://d33pksfia2a94m.cloudfront.net/assets/img/avatar/avatar_blank.png"
+        sx={{ width: 24, height: 24 }}
+      />
+    </Stack>
+  );
 }
