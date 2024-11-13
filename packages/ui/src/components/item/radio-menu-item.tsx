431a4c0f5f6a8208f512b41d68057d5dbfbdbdb8
minvistar <rlarnfk135@naver.com>
2024-11-14 01:09:40 +0900

    .

--- packages/ui/src/components/item/radio-menu-item.tsx
+++ packages/ui/src/components/item/radio-menu-item.tsx
@@ -1,7 +1,7 @@
 import { MenuItem, menuItemClasses, MenuItemProps, useRadioGroup, useTheme } from "@mui/material";
 import { MouseEventHandler, useCallback } from "react";
 
-type RadioMenuItemProps = MenuItemProps & { value: string };
+type RadioMenuItemProps = MenuItemProps<"button"> & { value: string };
 
 export function RadioMenuItem({ value, ...rest }: RadioMenuItemProps) {
   const radioGroup = useRadioGroup();
@@ -9,7 +9,7 @@ export function RadioMenuItem({ value, ...rest }: RadioMenuItemProps) {
 
   if (!radioGroup) throw new Error("<RadioMenuItem />의 부모로 <RadioGroup /> 컴포넌트가 있어야 합니다.");
 
-  const handleClick: MouseEventHandler<HTMLLIElement> = useCallback(
+  const handleClick: MouseEventHandler<HTMLButtonElement> = useCallback(
     (e) => {
       radioGroup.onChange(e as unknown as React.ChangeEvent<HTMLInputElement>, value);
     },
@@ -17,26 +17,27 @@ export function RadioMenuItem({ value, ...rest }: RadioMenuItemProps) {
   );
 
   return (
-    <MenuItem
-      {...rest}
-      {...{
-        component: "button",
-        name: radioGroup.name,
-      }}
-      value={value}
-      selected={value === radioGroup.value}
-      onClick={handleClick}
-      sx={{
-        px: "14px",
-        py: "8px",
-        fontWeight: theme.typography.fontWeightMedium,
-        color: theme.vars.palette.text.secondary,
-        fontSize: "15px",
-        lineHeight: "1.45",
-        [`&.${menuItemClasses.selected}`]: {
-          color: theme.vars.palette.text.primary,
-        },
-      }}
-    />
+    <>
+      <MenuItem
+        {...rest}
+        component="button"
+        name={radioGroup.name}
+        value={value}
+        selected={value === radioGroup.value}
+        onClick={handleClick}
+        sx={{
+          px: "14px",
+          py: "8px",
+          fontWeight: theme.typography.fontWeightMedium,
+          color: theme.vars.palette.text.secondary,
+          fontSize: "15px",
+          lineHeight: "1.45",
+          [`&.${menuItemClasses.selected}`]: {
+            color: theme.vars.palette.text.primary,
+          },
+          ...rest.sx,
+        }}
+      />
+    </>
   );
 }
