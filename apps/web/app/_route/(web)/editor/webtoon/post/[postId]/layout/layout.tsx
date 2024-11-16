459b716f47e0375e1e4e1797d83ae3c0a003e76d
Xiel0112 <shinhw1127@gmail.com>
2024-11-16 14:38:58 +0900

    .

--- apps/web/app/_route/(web)/editor/webtoon/post/[postId]/layout/layout.tsx
+++ apps/web/app/_route/(web)/editor/webtoon/post/[postId]/layout/layout.tsx
@@ -4,6 +4,7 @@ import { SimpleLayout } from "@pency/ui/layouts";
 import Left from "./left";
 import Right from "./right";
 import { useTheme } from "@mui/material";
+import { WT_Post_Create_Form } from "_core/webtoon/post";
 
 type Props = {
   children: React.ReactNode;
@@ -13,29 +14,31 @@ export function PostIdLayout({ children }: Props) {
   const theme = useTheme();
 
   return (
-    <SimpleLayout
-      slots={{
-        header: (
-          <SimpleLayout.Header
-            slots={{
-              left: <Left />,
-              right: <Right />,
-            }}
-          />
-        ),
-      }}
-      slotProps={{
-        container: {
-          sx: {
-            maxWidth: "740px",
-            [theme.breakpoints.up("lg")]: {
+    <WT_Post_Create_Form>
+      <SimpleLayout
+        slots={{
+          header: (
+            <SimpleLayout.Header
+              slots={{
+                left: <Left />,
+                right: <Right />,
+              }}
+            />
+          ),
+        }}
+        slotProps={{
+          container: {
+            sx: {
               maxWidth: "740px",
+              [theme.breakpoints.up("lg")]: {
+                maxWidth: "740px",
+              },
             },
           },
-        },
-      }}
-    >
-      {children}
-    </SimpleLayout>
+        }}
+      >
+        {children}
+      </SimpleLayout>
+    </WT_Post_Create_Form>
   );
 }
--- apps/web/app/_route/(web)/editor/webtoon/post/[postId]/page/page.tsx
+++ apps/web/app/_route/(web)/editor/webtoon/post/[postId]/page/page.tsx
@@ -8,24 +8,22 @@ import { WT_Post_Create_Form } from "_core/webtoon/post";
 export function PostIdPage() {
   const theme = useTheme();
   return (
-    <Stack spacing={4}>
-      <Typography variant="h4">포스트 발행하기</Typography>
-      <Box sx={{ backgroundColor: theme.vars.palette.background.paper, padding: 3, borderRadius: 2 }}>
+    <>
+      <Stack spacing={4} sx={{ backgroundColor: theme.vars.palette.background.paper, padding: 3, borderRadius: 2 }}>
+        <Typography variant="h4">포스트 발행하기</Typography>
         <form
           onSubmit={(e) => {
             e.preventDefault();
           }}
         >
-          <WT_Post_Create_Form>
-            <Stack spacing={4}>
-              <WT_Post_Create_Form.Title />
-              <WT_Post_Create_Form.Genre />
-              <Typography>미리보기</Typography>
-              <Typography>에디터</Typography>
-            </Stack>
-          </WT_Post_Create_Form>
+          <Stack spacing={4}>
+            <WT_Post_Create_Form.Title />
+            <WT_Post_Create_Form.Genre />
+            <Typography>미리보기</Typography>
+            <Typography>에디터</Typography>
+          </Stack>
         </form>
-      </Box>
-    </Stack>
+      </Stack>
+    </>
   );
 }
