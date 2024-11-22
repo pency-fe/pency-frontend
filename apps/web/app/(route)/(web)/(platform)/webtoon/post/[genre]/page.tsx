9b42e052c8937fe9b0f0424fefa9617ef1526e8f
Xiel0112 <shinhw1127@gmail.com>
2024-11-22 16:16:10 +0900

    .

---
+++ apps/web/app/(route)/(web)/(platform)/webtoon/post/[genre]/page.tsx
@@ -0,0 +1,5 @@
+import GenrePage from "@/_route/(web)/(platform)/webtoon/post/page/[genre]/page/page";
+
+export default function Page() {
+  return <GenrePage />;
+}
---
+++ apps/web/app/_route/(web)/(platform)/webtoon/post/page/[genre]/page/page.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import {
+  Stack,
+  Box,
+  Typography,
+  TextField,
+  inputBaseClasses,
+  MenuItem,
+  Grid,
+  Pagination,
+  useTheme,
+} from "@mui/material";
+import { WT_Post_RichCard } from "_core/webtoon/post";
+
+export default function GenrePage() {
+  const theme = useTheme();
+  type Sort = {
+    options: { value: string; label: string }[];
+  };
+  const sort: Sort = {
+    options: [
+      { value: "latest", label: "최신순" },
+      { value: "popularity", label: "전체 인기순" },
+      { value: "popularity", label: "주간 인기순" },
+    ],
+  };
+
+  return (
+    <Stack spacing={2}>
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        <Typography variant="h4">웹툰 포스트</Typography>
+
+        <Box ml="auto">
+          <TextField
+            select
+            defaultValue={sort.options[0]?.value}
+            size="small"
+            sx={{
+              fontSize: 13,
+              [`& .${inputBaseClasses.root}`]: {
+                [theme.breakpoints.up("xs")]: { height: 34 },
+              },
+              [`& .${inputBaseClasses.input}`]: {
+                [theme.breakpoints.up("xs")]: { fontSize: 12 },
+                [theme.breakpoints.up("sm")]: { fontSize: 14 },
+              },
+            }}
+          >
+            {sort.options.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Box>
+      </Box>
+      <Grid container spacing={1}>
+        {Array.from({ length: 12 }, (_, index) => (
+          <Grid item key={index} xs={15} sm={6} md={4} lg={3}>
+            <WT_Post_RichCard
+              data={{
+                postId: "1",
+                thumbnail:
+                  "https://page-images.kakaoentcdn.com/download/resource?kid=b2PvT7/hAFPPPhF6U/e8nt8ArmKwQnOwsMS6TTFk&filename=o1",
+                age: "ALL",
+                price: 100,
+                purchased: false,
+                creationType: "PRIMARY",
+                pair: "NONE",
+                genre: "ROMANCE",
+                title: "천재 궁수의 스트리밍",
+                channel: {
+                  channelId: "123",
+                  avatar: "https://d33pksfia2a94m.cloudfront.net/assets/img/avatar/avatar_blank.png",
+                  name: "김천재",
+                },
+                likeCount: 0,
+                createdAt: 0,
+                keywords: ["환생", "판타지", "BJ", "미남", "너드"],
+                preview:
+                  "천재 궁수의 스트리밍 천재 궁수의 스트리밍 천재 궁수의 스트리밍 천재 궁수의 스트리밍 천재 궁수의 스트리밍 천재 궁수의 스트리밍 천재 궁수의 스트리밍 천재 궁수의 스트리밍 천재 궁수의 스트리밍 천재 궁수의 스트리밍 천재 궁수의 스트리밍 천재 궁수의 스트리밍 ",
+              }}
+            />
+          </Grid>
+        ))}
+      </Grid>
+      <Box sx={{ margin: "auto", mt: 3 }}>
+        <Pagination count={10} />
+      </Box>
+    </Stack>
+  );
+}
