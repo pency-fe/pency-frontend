d136773ffa0a39d1943bf3813e958e9997221fde
Xiel0112 <shinhw1127@gmail.com>
2024-12-03 16:53:35 +0900

    .

--- apps/web/app/(route)/(web)/(webtoon-viewer)/[channelUrl]/webtoon/post/[postId]/_page/page.tsx
+++ apps/web/app/(route)/(web)/(webtoon-viewer)/[channelUrl]/webtoon/post/[postId]/_page/page.tsx
@@ -1,19 +1,20 @@
 "use client";
 
-import { Stack } from "@mui/material";
+import { Slide, Stack, useScrollTrigger } from "@mui/material";
 import { Header, Main } from "@pency/ui/layouts";
 import { HeaderLeft } from "./header-left";
 import { HeaderRight } from "./header-right";
 import { ViewerSection } from "./viewer-section";
 import { BottomAppBar } from "./bottom-app-bar";
 import { EtcSection } from "./etc-section";
+import { ReactElement } from "react";
 
 // ----------------------------------------------------------------------
 
 export function PostIdPage() {
   return (
     <>
-      <Header slots={{ left: <HeaderLeft />, right: <HeaderRight /> }} />
+      <HideTopAppBar />
       <Main>
         <Stack spacing={1} sx={{ maxWidth: "700px" }}>
           <ViewerSection />
@@ -22,5 +23,34 @@ export function PostIdPage() {
       </Main>
       <BottomAppBar />
     </>
+  );
+}
+
+// ----------------------------------------------------------------------
+
+type HideOnScrollProps = {
+  window?: () => Window;
+  children?: ReactElement<unknown>;
+};
+
+function HideOnScroll({ children, window }: HideOnScrollProps) {
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children ?? <div />}
+    </Slide>
+  );
+}
+
+function HideTopAppBar(props: HideOnScrollProps) {
+  return (
+    <HideOnScroll {...props}>
+      <Header slots={{ left: <HeaderLeft />, right: <HeaderRight /> }} />
+    </HideOnScroll>
   );
 }
