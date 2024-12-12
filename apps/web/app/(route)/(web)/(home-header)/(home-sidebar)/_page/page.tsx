9190058988de1fa55731a32ba05bd16d519389bc
minvistar <rlarnfk135@naver.com>
2024-12-12 16:30:11 +0900

    .

--- apps/web/app/(route)/(web)/(home-header)/(home-sidebar)/_page/page.tsx
+++ apps/web/app/(route)/(web)/(home-header)/(home-sidebar)/_page/page.tsx
@@ -10,32 +10,34 @@ export default function HomeSidebarPage() {
   const theme = useTheme();
 
   return (
-    <Grid container spacing={4}>
-      <Grid item xs={12}>
-        <BannerSection />
-      </Grid>
-      <Grid item xs={12}>
-        <Stack direction="row" alignItems="center" sx={{ mb: 1.5 }}>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            주간 인기 웹툰
-          </Typography>
-          <Button
-            component={NextLink}
-            href="/webtoon/series?TODO=TODO"
-            size="small"
-            color="inherit"
-            sx={{
-              color: theme.vars.palette.grey[500],
-              [stylesColorScheme.dark]: {
+    <>
+      <Grid container spacing={4}>
+        <Grid item xs={12}>
+          <BannerSection />
+        </Grid>
+        <Grid item xs={12}>
+          <Stack direction="row" alignItems="center" sx={{ mb: 1.5 }}>
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>
+              주간 인기 웹툰
+            </Typography>
+            <Button
+              component={NextLink}
+              href="/webtoon/series?TODO=TODO"
+              size="small"
+              color="inherit"
+              sx={{
                 color: theme.vars.palette.grey[500],
-              },
-            }}
-          >
-            더 보기
-          </Button>
-        </Stack>
-        <WeekPopularWebtoonSection />
+                [stylesColorScheme.dark]: {
+                  color: theme.vars.palette.grey[500],
+                },
+              }}
+            >
+              더 보기
+            </Button>
+          </Stack>
+          <WeekPopularWebtoonSection />
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 }
