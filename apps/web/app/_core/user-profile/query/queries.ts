3f4631ef446294be710cd736655f273b509565d8
Xiel0112 <shinhw1127@gmail.com>
2024-12-15 23:52:35 +0900

    .

--- apps/web/app/_core/user-profile/query/queries.ts
+++ apps/web/app/_core/user-profile/query/queries.ts
@@ -1,4 +1,12 @@
+import { queryOptions } from "@tanstack/react-query";
+import { getUserProfileMeList } from "./api";
+
 export const userProfileMeKeys = {
   all: ["userProfileMe"],
   lists: () => [...userProfileMeKeys.all, "list"],
+  list: () =>
+    queryOptions<Awaited<ReturnType<typeof getUserProfileMeList>>>({
+      queryKey: [...userProfileMeKeys.lists()],
+      queryFn: () => getUserProfileMeList(),
+    }),
 };
