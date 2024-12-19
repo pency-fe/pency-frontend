2aabcedcce0d5885d962faf8eec042c3dcafa2fe
minvistar <rlarnfk135@naver.com>
2024-12-20 01:53:45 +0900

    .

--- apps/web/app/(route)/(web)/studio/[channelUrl]/setting/link/_page/page.tsx
+++ apps/web/app/(route)/(web)/studio/[channelUrl]/setting/link/_page/page.tsx
@@ -14,13 +14,19 @@ export const SettingLinkPage = withAsyncBoundary(SettingLinkPageFn, {
 
 function SettingLinkPageFn() {
   const url = useChannelUrlParam();
+  // 대기, 성공, 실패 -> 상태(state) -> 바뀔 때마다 리렌더링이 발생해. (기본값)
+  // 대기 -> 실패
+  // - 에러를 발생시켜서 위에서 에러를 처리할거다~~ 에러를 처리할 수 있는 컴포넌트는 ErrorBoundary야. 만약에 ErrorBoundary가 없다면, 최상위까지 올라서 사이트 전체가 먹통이 된다. throwOnError: true
+  // - 에러를 발생시키지 않고 지금 컴포넌트에서 에러를 처리할 거다. throwOnError: true가 없다는 거야. (throwOnError: undefined)
+  // 에러 발생시키지 않았기 때문에 리렌더링이 발생해.
+  // 내가 항상 정답을 알려줘서 이렇게 된건가?
+  // 꼼꼼하게 보고 -> 문법 모르는거 있으면 바로 공부하고 -> 실행 순서, 동작 과정 생각/공부/질문
   const query = useQuery({ ...channelMeKeys.linkDetail({ url }), throwOnError: true });
 
   if (query.status !== "success") {
     return <Loading />;
   }
 
-  // [TODO]
   const links = useMemo(() => {
     const linkObj: Record<string, string> = {};
     for (const data of query.data) {
