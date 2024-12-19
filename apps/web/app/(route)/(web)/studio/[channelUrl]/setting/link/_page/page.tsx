178a280af6c5ae36fdc5fbfa27538cfc11ba091f
Xiel0112 <shinhw1127@gmail.com>
2024-12-20 01:09:23 +0900

    .

--- apps/web/app/(route)/(web)/studio/[channelUrl]/setting/link/_page/page.tsx
+++ apps/web/app/(route)/(web)/studio/[channelUrl]/setting/link/_page/page.tsx
@@ -5,6 +5,7 @@ import { withAsyncBoundary } from "@pency/util";
 import { useQuery } from "@tanstack/react-query";
 import { CH_Link_Form, channelMeKeys } from "_core/channel";
 import { useChannelUrlParam } from "_hooks";
+import { useMemo } from "react";
 
 // ----------------------------------------------------------------------
 export const SettingLinkPage = withAsyncBoundary(SettingLinkPageFn, {
@@ -15,34 +16,42 @@ function SettingLinkPageFn() {
   const url = useChannelUrlParam();
   const query = useQuery({ ...channelMeKeys.linkDetail({ url }), throwOnError: true });
 
+  if (query.status !== "success") {
+    return <Loading />;
+  }
+
+  // [TODO]
+  const links = useMemo(() => {
+    const linkObj: Record<string, string> = {};
+    for (const data of query.data) {
+      linkObj[data.linkType.toLowerCase()] = data.url;
+    }
+
+    return linkObj;
+  }, [query.data]);
+
   return (
-    <>
-      {query.isPending ? (
-        <Loading />
-      ) : (
-        <CH_Link_Form>
-          <Stack spacing={3}>
-            <List>
-              <Stack spacing={2}>
-                <ListItem disablePadding>
-                  <CH_Link_Form.Home />
-                </ListItem>
+    <CH_Link_Form home={links.home} twitter={links.twitter} instagram={links.instagram}>
+      <Stack spacing={3}>
+        <List>
+          <Stack spacing={2}>
+            <ListItem disablePadding>
+              <CH_Link_Form.Home />
+            </ListItem>
 
-                <ListItem disablePadding>
-                  <CH_Link_Form.Twitter />
-                </ListItem>
+            <ListItem disablePadding>
+              <CH_Link_Form.Twitter />
+            </ListItem>
 
-                <ListItem disablePadding>
-                  <CH_Link_Form.Instagram />
-                </ListItem>
-              </Stack>
-            </List>
-
-            <CH_Link_Form.UpdateSubmit channelUrl={url} />
+            <ListItem disablePadding>
+              <CH_Link_Form.Instagram />
+            </ListItem>
           </Stack>
-        </CH_Link_Form>
-      )}
-    </>
+        </List>
+
+        <CH_Link_Form.UpdateSubmit channelUrl={url} />
+      </Stack>
+    </CH_Link_Form>
   );
 }
 
