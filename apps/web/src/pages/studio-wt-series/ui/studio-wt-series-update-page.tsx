9a4755e408c1a115f990157ffcf4d63e36a2466d
Xiel0112 <shinhw1127@gmail.com>
2025-01-04 23:20:34 +0900

    .

--- apps/web/src/pages/studio-wt-series/ui/studio-wt-series-update-page.tsx
+++ apps/web/src/pages/studio-wt-series/ui/studio-wt-series-update-page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { WtSeriesForm } from "@/features/wt-series-me";
-import { Box, Grid, Stack } from "@mui/material";
+import { Box, Grid, Skeleton, Stack } from "@mui/material";
 
 export const StudioWtSeriesUpdatePage = () => {
   return (
@@ -32,5 +32,27 @@ export const StudioWtSeriesUpdatePage = () => {
 };
 
 const Loading = () => {
-  return;
+  return (
+    <Stack spacing={3}>
+      <Grid container spacing={4}>
+        <Grid item xs={12} md="auto">
+          <Stack spacing={1} sx={{ alignItems: "center" }}>
+            <Skeleton width={260} height={212} />
+          </Stack>
+        </Grid>
+        <Grid item xs={12} md>
+          <Stack spacing={4}>
+            <Skeleton width="100%" height={66} />
+            <Skeleton width="100%" height={56} />
+            <Skeleton width="100%" height={80} />
+            <Skeleton width="100%" height={117} />
+            <Skeleton width="100%" height={110} />
+          </Stack>
+        </Grid>
+      </Grid>
+      <Box sx={{ display: "flex", justifyContent: "flex-end", gap: 1 }}>
+        <Skeleton width={115} height={36} />
+      </Box>
+    </Stack>
+  );
 };
